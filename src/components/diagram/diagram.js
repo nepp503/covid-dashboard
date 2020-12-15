469a8976e39b74e53bbf12eee16047e630fd8719
aleksandrelius <aleksandreliuz@gmail.com>
2020-12-15 22:23:52 +0300

    feat: add map with default functionality

--- src/components/diagram/diagram.js
+++ src/components/diagram/diagram.js
@@ -4,5 +4,10 @@ import React from 'react';
 
 export default class Diagram extends React.Component {
 
+render() {
+    return (
+        <div></div>
+    )
+}
 
 }
--- src/components/map/map.js
+++ src/components/map/map.js
@@ -1,8 +1,45 @@
-import "map.css"
+import "./map.css"
 import React from "react";
+import { MapContainer, TileLayer, Marker } from 'react-leaflet'
+import DataLoader from "../../service/dataLoader";
+
 
 
 export default class Map extends React.Component {
 
+    state = {
+        countryList: null
+    }
 
+    dataLoader = new DataLoader();
+
+    viewCountries = (toggleCountries) => {
+        toggleCountries()
+            .then(countryList => {
+                this.setState({
+                    countryList
+                })
+            })
+    }
+
+    componentDidMount() {
+        const { toggleCountries } = this.props;
+
+        setTimeout(this.viewCountries(toggleCountries), 2000)
+    }
+
+    render() {
+        return (
+        // <div></div> 
+            <MapContainer center={[51.505, -0.09]} zoom={13} scrollWheelZoom={false}>
+            <TileLayer
+              attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+              url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+            />
+            <Marker position={[51.505, -0.09]}>
+
+            </Marker>
+          </MapContainer>
+        )
+    }
 }
