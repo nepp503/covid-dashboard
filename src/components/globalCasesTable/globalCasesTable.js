8f1a3451a76f541c8429972d0580024a657fa9dd
FullCycleRoid <andrewthechf@gmail.com>
2020-12-16 11:41:33 +0300

    feat: case buttons

--- src/components/globalCasesTable/globalCasesTable.js
+++ src/components/globalCasesTable/globalCasesTable.js
@@ -1,43 +1,40 @@
 import "./globalCasesTable.css"
 import React from "react";
+import ReactDOM from 'react-dom';
 import DataLoader from "../../service/dataLoader";
 import TableItem from "../tableItem";
-import InputItem from "../inputItem";
+import InputItem from "../../inputItem";
 import MaterialIcon from 'material-icons-react';
 
 export default class GlobalCasesTable extends React.Component {
 
     dataLoader = new DataLoader()
 
-  constructor() {
-    super()
-    this.state = {
+    state = {
         topCasesList: null,
         loader: true,
         error: false,
         caseType: "cases",
     }
-  }
 
     caseTypeSwitcher = (e) => {
-        console.log(e.currentTarget.id)
+        console.log(this.state.caseType)
         this.setState({
             caseType: e.currentTarget.id
         })
-        console.log(this.state.caseType)
     }
-
 
     render() {
       return (
           <div className="globalCasesTable">
-            <InputItem />
             <h2>Cases by country/region</h2>
+            <InputItem />
             <div className="case-switcher">
                 <div className="case-switcher__item" id="cases" onClick={this.caseTypeSwitcher}><MaterialIcon title="Cases" icon="work" color='#ffffff' size={30}/></div>
                 <div className="case-switcher__item" id="recovered" onClick={this.caseTypeSwitcher}><MaterialIcon title="recovered" icon="health_and_safety" color='#ffffff' size={30}/></div>
                 <div className="case-switcher__item" id="deaths" onClick={this.caseTypeSwitcher}><MaterialIcon title="deaths" icon="report_problem" color='#ffffff' size={30}/></div>
             </div>
+
             <TableItem
                 caseType = {this.state.caseType}
                 getTableItems = {this.dataLoader.getSortedCountries}
