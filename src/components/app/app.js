a59c4fc227c5fc6edaf83d49bc0a4ac146edda32
aleksandrelius <aleksandreliuz@gmail.com>
2020-12-23 07:40:45 +0300

    feat: add some adaptive to the dashboard

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -3,10 +3,10 @@ import './app.css';
 import GlobalCases from "../globalCases";
 import Map from "../map";
 import Header from "../header";
-import Diagram from "../diagram";
 import CountryInfoTable from "../countryInfoTable";
 import GlobalCasesTable from "../globalCasesTable";
 import DataLoader from "../../service/dataLoader";
+import DiagramBoard from '../diagram/diagramBoard';
 
 export default class App extends React.Component {
 
@@ -24,7 +24,7 @@ export default class App extends React.Component {
     }
 
     shouldComponentUpdate(nextProps, nextState, nextContext) {
-      return this.state.selectedCountryObj = nextState.selectedCountryObj
+      return this.state.selectedCountryObj = nextState.selectedCountryObj;
     }
 
     render() {
@@ -47,7 +47,7 @@ export default class App extends React.Component {
                           selectedCountryObj = {this.state.selectedCountryObj}
                           getWorldStats         = {this.dataLoader.getWorldStats}
                         />
-                        <Diagram />
+                        <DiagramBoard selectedCountryObj = {this.state.selectedCountryObj}/>
                     </div>
                 </div>
             </div>
--- src/components/diagram/diagram.js
+++ src/components/diagram/diagram.js
@@ -17,33 +17,34 @@ export default class Diagram extends React.Component {
 
     componentDidMount() {
         this.myChart = new Chart(this.chartRef.current, {
-          type: 'bar',
-          data: {
-              datasets: [{
-                  data: [{x:'2016-10-25', y:20}, {x:'2016-11-25', y:15}, {x:'2016-12-26', y:10}],
-                  backgroundColor: [
-                      'rgba(255, 99, 132, 0.2)',
-                      'rgba(54, 162, 235, 0.2)',
-                  ],
-                  borderColor: [
-                      'rgba(255, 99, 132, 1)',
-                      'rgba(54, 162, 235, 1)',
-                  ],
-                  borderWidth: 1,
-                  barPercentage: 1,
-                  categoryPercentage: 1.0
-              }]
-          },
-          options: {
-            scales: {
-                xAxes: [{
-                    type: 'time',
-                    time: {
-                        unit: 'day'
-                    }
+            type: 'bar',
+            data: {
+                datasets: [{
+                    label: this.props.cases,
+                    data: this.props.data,
+                    backgroundColor: 'white',
+                    barPercentage: 1,
+                    categoryPercentage: 1,
+                    borderWidth: 0
                 }]
+            },
+            options: {
+                scales: {
+                    xAxes: [{
+                        type: 'time',
+                        time: {
+                            unit: 'quarter'
+                        }
+                    }]
+                }
             }
-        }
         });
-      }
+    }
+
+    componentDidUpdate(){
+        this.myChart.data.datasets[0].data = this.props.data;
+        this.myChart.data.datasets[0].backgroundColor = Array(this.props.data.length).fill('white');
+        this.myChart.data.datasets[0].label = this.props.cases;
+        this.myChart.update();
+    }
 }
